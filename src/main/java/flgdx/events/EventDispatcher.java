48537b5992b9cd8d897b8d7360b86824d275c025
cleversoap <clever@cleversoap.com>
2014-02-20 07:45:49 +1000

    Basic EventDispatcher is in place. Currently using an EventDispatcherPair class to wrap the reflection based retrieval of methods from listeners.

--- src/main/java/flgdx/events/EventDispatcher.java
+++ src/main/java/flgdx/events/EventDispatcher.java
@@ -1,24 +1,58 @@
 package flgdx.events;
 
+import java.util.Hashtable;
+import java.util.ArrayList;
+
 public class EventDispatcher implements IEventDispatcher
 {
+    // TODO:    Dictionary that is keyed by the event type
+    //          but that holds a threadsafe list of event
+    //          handlers and the method (some sort of EventListenerPair
+    //          class may be needed)
+    protected Hashtable<String, ArrayList<EventListenerPair> > _listeners;
+
     public EventDispatcher()
     {
+        _listeners = new Hashtable<String, ArrayList<EventListenerPair> >();
     }
 
     public void addEventListener(String type, Object listener, String method)
     {
+        // If there is no list of listeners for this key
+        // then create one.
+        if (!_listeners.containsKey(type))
+        {
+            _listeners.put(type, new ArrayList<EventListenerPair>());
+        }
 
+        // Now add to the entry
+        _listeners.get(type).add(new EventListenerPair(listener, method));
     }
 
     public boolean dispatchEvent(Event event)
     {
-        return true;
+        boolean result = true;
+
+        // TODO: Fastest way to iterate this list
+        if (this.hasEventListener(event.type))
+        {
+            for (EventListenerPair listener : _listeners.get(event.type))
+            {
+                result = listener.call(event);
+
+                if (!result)
+                {
+                    break;
+                }
+            }
+        }
+
+        return result;
     }
 
     public boolean hasEventListener(String type)
     {
-        return false;
+        return _listeners.containsKey(type);
     }
 
     public void removeEventListener(String type, Object listener, String method)
@@ -26,8 +60,9 @@ public class EventDispatcher implements IEventDispatcher
 
     }
 
+    // TODO: This will need to be a part of an event chain if implemented.
     public boolean willTrigger(String type)
     {
-        return false;
+        return this.hasEventListener(type);
     }
 }
---
+++ src/main/java/flgdx/events/EventListenerPair.java
@@ -0,0 +1,53 @@
+package flgdx.events;
+
+import java.lang.reflect.Method;
+
+public class EventListenerPair
+{
+    protected Object _listener;
+    protected Method _method;
+
+    public EventListenerPair(Object listener, String method)
+    {
+        try
+        {
+            _listener = listener;
+            for (Method m : listener.getClass().getMethods())
+            {
+                if (m.getName().equals(method))
+                {
+                    _method = m;
+                    break;
+                }
+            }
+
+            if (_method == null)
+            {
+                throw new Exception("Could not find method named " + method + " in class " + listener.getClass().getName());
+            }
+        }
+        catch(Exception ex)
+        {
+            // Write error
+        }
+    }
+
+    public boolean call(Event event)
+    {
+        try
+        {
+            _method.invoke(_listener, event);
+            return true;
+        }
+        catch(Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public void dispose()
+    {
+        _listener = null;
+        _method = null;
+    }
+}
